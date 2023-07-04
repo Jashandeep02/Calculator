10cdf2d375ad118fde35b6459bcd5a2a91f9e69a
Jashandeep Singla <jashandeepsingla24@gmail.com>
2023-07-04 21:09:50 +0530

    Calculator using javascript

---
+++ calculator.js
@@ -0,0 +1,31 @@
+function handleClick(value) {
+  document.getElementById('display').value += value;
+}
+
+function calculate() {
+  var expression = document.getElementById('display').value;
+
+  try {
+    var result = eval(expression);
+    document.getElementById('display').value = result;
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
+  }
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+document.onkeydown = function(event) {
+  var key = event.key;
+
+  if ((key >= '0' && key <= '9') || key === '.' || key === '+' || key === '-' || key === '*' || key === '/') {
+    document.getElementById('display').value += key;
+  } else if (key === '=') {
+    calculate();
+  }
+  else if (key === 'C' || key === 'c'){
+    clearDisplay();
+  }
+}
